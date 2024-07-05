ee1cb81d45beaa870e73cecadd4ab03f1c63a1fb
Aithan Eulysse Gimenez <gimenezaithan@gmail.com>
2024-07-05 10:22:58 +0800

    finished sanctum activity

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,6 @@ Route::middleware('auth:sanctum')->group(function () {
 });
 
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
